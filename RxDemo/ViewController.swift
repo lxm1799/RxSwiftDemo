a7c24a5be595a3ca17317ea95d406484128f49d7
mac <goodluck1130@163.com>
2021-10-14 19:00:48 +0800

    提交

--- RxDemo/ViewController.swift
+++ RxDemo/ViewController.swift
@@ -9,49 +9,45 @@ import UIKit
 import RxSwift
 import RxCocoa
 
-private let minimalUsernameLength = 5
-private let minimalPasswordLength = 5
+let maxCount = 10
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var nameTextField: UITextField!
-    @IBOutlet weak var userNameLab: UILabel!
-    
-    @IBOutlet weak var loginLab: UILabel!
-    @IBOutlet weak var pwdTextField: UITextField!
-    
-    @IBOutlet weak var loginBtn: UIButton!
-    
     let disposeBag = DisposeBag.init()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        let lab = UILabel.init(frame: .init(x:(view.frame.width - 100)/2, y: 50, width: 100, height: 30))
+        lab.textColor = .black
+        view.addSubview(lab)
         
-        let userNameValid = nameTextField.rx.text.orEmpty
-            .map{$0.count >= minimalUsernameLength}
-            .share(replay: 1)
-        
-        let pwdValid = pwdTextField.rx.text.orEmpty
-            .map{$0.count >= minimalPasswordLength}
-            .share(replay: 1)
-        
-        let everythingValid = Observable.combineLatest(userNameValid,pwdValid){$0 && $1}
-            .share(replay: 1)
-        
-        userNameValid
-            .bind(to: loginLab.rx.isEnabled)
+        let textfiled = UITextField.init(frame: .init(x: 0, y: 0, width: 200, height: 40))
+        textfiled.center = view.center
+        textfiled.textColor = .black
+        textfiled.backgroundColor = .red
+        textfiled.borderStyle = .roundedRect
+        textfiled.rx.text.orEmpty.asObservable()
+            .subscribe(onNext:{
+                print("text1 = \($0)  count1 = \($0.count)")
+                let count = $0.count
+                if count > maxCount{
+                    let index = $0.index($0.startIndex, offsetBy: maxCount)
+                    let text = $0.substring(to: index)
+                    textfiled.text = text
+                    print("text2 = \(text)  count2 = \(text.count)")
+                }
+            })
             .disposed(by: disposeBag)
         
-        userNameValid
-            .bind(to: userNameLab.rx.isHidden)
-            .disposed(by: disposeBag)
-        
-        
-        
-        pwdValid
-            .bind(to: pwdTextField.rx.isHidden)
-            .disposed(by: disposeBag)
+        ///绑定到其他view上
+        textfiled.rx.text.bind(to: lab.rx.text).disposed(by: disposeBag)        
 
+        view.addSubview(textfiled)
+        
+        
+        
+        
     }
 
 
